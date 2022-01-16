0f202fa4a303dcf3f93096cf81107e1beef893b7
yoonyoung <yylee2021@gmail.com>
2022-01-16 14:29:00 +0900

    initial commit

---
+++ src/test/java/com/nowworks/groupware/GroupwareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nowworks.groupware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroupwareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
